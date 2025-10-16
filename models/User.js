14fefafc803f1cafe9a9835a88d1682e6b317ff0
JoiceGloriya <joicegloriyaa@gmail.com>
2025-10-16 20:17:42 +0530

    Trigger Azure deployment

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: String,
   name: String,
+  email: String,
 });
 
 module.exports = mongoose.model("User", userSchema);
